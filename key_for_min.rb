5097d0a3331a03da69c562721cc9f8e1569f0845
Elle Faraday <faraday.elle@gmail.com>
2017-11-01 01:45:48 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   key_for_min_value = nil
-  smallest_value = nil
+  smallest_value 
   name_hash.each do |key, value|
     if value < smallest_value
       key_for_min_value = key
